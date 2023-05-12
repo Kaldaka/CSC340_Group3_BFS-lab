9b033bf8cc7dfe6352ed7ccfca3ec9a92a3ecfd3
Kaldaka <elliotjwarren@gmail.com>
2023-05-12 11:48:59 -0700

    Created boilerplate methods and files

---
+++ BFS_algo_main.cpp
@@ -0,0 +1,21 @@
+/***
+CSC340_Group3_BFS - lab.cpp
+Last lab of CSC340 at SFSU
+Uses Breadth-first search algorithm to explore tree structures
+
+Created by:
+    Abdarrahman Ayyaz
+    Dyllan Grossman
+    Elliot Warren
+    Mohammed Deeb
+***/
+
+#include "BFS_algo.h"
+using namespace NS_BFS_ALGO;
+
+int main()
+{
+    //call BFS method and mutators 
+    //print out tree from given vertex
+    //print shortest bath from source vertex (s) to destination vertex (v) using recursive algorithm 
+}
