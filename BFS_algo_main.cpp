05c0a1cae8d4bd8a3e7bd5c08ea803b94b69cb8b
TheRealDyllanGrossman <111920073+TheRealDyllanGrossman@users.noreply.github.com>
2023-05-15 16:12:45 -0700

    Update BFS_algo_main.cpp

--- BFS_algo_main.cpp
+++ BFS_algo_main.cpp
@@ -19,4 +19,18 @@ int main()
     //call BFS method and mutators 
     //print out tree from given vertex
     //print shortest bath from source vertex (s) to destination vertex (v) using recursive algorithm 
+    
+    Graph g(5);
+    
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+
+    g.BFS(0);
+    std::cout << "\nShortest path from 0 to 4: ";
+    g.printPath(0, 4);
+
+    return 0;
+    
 }
