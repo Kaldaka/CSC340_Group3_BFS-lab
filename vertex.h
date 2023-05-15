7928e267d9548e36238f501b05e55254128e5feb
TheRealDyllanGrossman <111920073+TheRealDyllanGrossman@users.noreply.github.com>
2023-05-15 16:21:12 -0700

    Update vertex.h
    Preparing for full implementation of Graph.cpp

--- vertex.h
+++ vertex.h
@@ -8,25 +8,17 @@ namespace NS_BFS_ALGO {
 	class Vertex {
 
 		private:
-
-			int identifier = 0;
-			std::string color = "na";
-			int predecessor = 0;
+			int num_vertices;
+			std::vector<Vertex*> vertices;
+			std::vector<std::list<Vertex*>> adj;
 
 		public:
+			Graph(int num_vertices);
+		    	~Graph();
 
-			Vertex();
-			Vertex(int identifier, std::string color, int predecessor);
-
-			//mutators
-			void setIdentifier(int newIdentifier);
-			void setColor(std::string newColor);
-			void setPredecessor(int newPredecessor);
-
-			//accessors
-			int getIdentifier();
-			std::string getColor();
-			int getPredecessor();
+			void addEdge(int u, int v);
+			void BFS(int s);
+			void printPath(int s, int v);
 	};
 }
 
