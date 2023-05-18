3d1aea1dee3452bbc3e31fbd109379cc0bb3b52e
Elliot <elliotjwarren@gmail.com>
2023-05-18 13:18:21 -0700

    changed object instantiation and method calls to reflect current itteration

--- BFS_algo_main.cpp
+++ BFS_algo_main.cpp
@@ -10,8 +10,8 @@ Created by:
     Mohammed Deeb
 ***/
 
-#include "graph.h"
-#include "vertex.h"
+#include "Graph.h"
+#include "Vertex.h"
 using namespace NS_BFS_ALGO;
 
 int main()
@@ -20,12 +20,29 @@ int main()
     //print out tree from given vertex
     //print shortest bath from source vertex (s) to destination vertex (v) using recursive algorithm 
     
-    Graph g(5);
+    Graph g;
+
+    Vertex v0(0);
+    Vertex v1(1);
+    Vertex v2(2);
+    Vertex v3(3);
+    Vertex v4(4);
+    Vertex v5(5);
     
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 3);
-    g.addEdge(2, 4);
+    g.addVertex(v0);
+    g.addVertex(v1);
+    g.addVertex(v2);
+    g.addVertex(v3);
+    g.addVertex(v4);
+    g.addVertex(v5);
+
+    g.addAdjacentVertex(1, v3);
+    g.addAdjacentVertex(2, v3);
+    g.addAdjacentVertex(4, v3);
+    g.addAdjacentVertex(0, v1);
+    g.addAdjacentVertex(3, v0);
+    g.addAdjacentVertex(5, v2);
+    g.addAdjacentVertex(1, v5);
 
     g.BFS(0);
     std::cout << "\nShortest path from 0 to 4: ";
